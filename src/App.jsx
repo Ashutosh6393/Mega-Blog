0362aa303af3640daa2040ace8fd06c545c230e6
Ashutosh <vashutosh625@gmail.com>
2024-02-05 22:53:21 +0530

    added all backend services of appwrite

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import "./App.css";
+import conf from "./conf/conf";
 function App() {
-  const [count, setCount] = useState(0)
-
+  console.log(conf.appwriteDatabaseId);
+  console.log(conf.appwriteCollectionId);
   return (
     <>
-     <h1>this is a blog website</h1>
+      <h1>this is a blog website</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
