9a574820e1e1b80ad68e3b32a4c084cdcfe41901
Ashutosh <vashutosh625@gmail.com>
2024-02-29 00:45:26 +0530

    updated

---
+++ src/components/Login.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { login as authLogin } from "../store/authSlice";
+import {Button, Input, Logo } from './index';
+import {useDispatch} from '../appwrite/auth';
+import {useForm} from 'react-hook-form'
+
+
+function Login() {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const {register, handleSubmit} = useForm();
+    const [error, setError ] = useState("");
+
+
+    const login = async(data) =>{
+        setError("")
+    }
+
+
+  return <div>Login</div>;
+}
+
+export default Login;
